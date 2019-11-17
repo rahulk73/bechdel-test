f56eacc72ea73e622a119869b16b02330defee2f
Mathis Renier <reniermathis@gmail.com>
2019-11-17 12:01:03 -0500

    cleaned client.py

--- src/Client.py
+++ src/Client.py
@@ -6,11 +6,15 @@ from src.piechart import *
 castlist = CastList("../Scripts/TheHelp.pdf")
 menDict = castlist.get_male()
 femDict = castlist.get_female()
-PieChart(menDict, femDict)
 script = castlist.get_dialogues()
 all_conv = get_satisfying_conversations(script, menDict, femDict)
 all_conv_sliced = slice_conversation(all_conv)
 
+
+# generate the pie chart
+PieChart(menDict, femDict)
+
+# perform tests
 assert pass_test1(femDict), "Test1: Failed"
 assert pass_test2(script, menDict, femDict), "Test2: Failed"
 assert pass_test3(all_conv, menDict), "Test3: Failed"
